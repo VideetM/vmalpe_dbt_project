e8f77acf14648418a24f21ac7a139b80786131eb
vids107@gmail.com <vids107@gmail.com>
2023-05-02 00:23:59 +0000

    build intial jaffle shop data

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from raw.stripe.payment
+
+),
+
+staged as (
+
+    select id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        (amount/100) as payment_amount_usd,
+        created as created_date
+
+
+    from source
+)
+
+select * from staged
